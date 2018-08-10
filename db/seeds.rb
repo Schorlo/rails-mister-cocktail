27d8cc7c5e123a75cf3a6d7731d394545d391d5e
Nicolas Schorlo <n.vonschorlemer@whu.edu>
2018-08-10 17:23:55 +0200

    horoku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "wodka")
+
+
+
+
+# dose = Dose.new(description: '2 shots')
+# dose.valid?
+# dose.errors.messages
+
+# dose.save
